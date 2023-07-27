5288ae2b342c2fb0827677aa479282fdcc43f66c
kai._.mjy <kaimjy@kaist.ac.kr>
2023-07-27 13:40:54 +0900

    2023-07-26 22:31 jiyeon
    일단 bookmarklist 불러오기 완^^

--- lib/scraped.dart
+++ lib/scraped.dart
@@ -1,46 +1,188 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Article {
-  final String title;
-  final String description;
+String baseUrl = "http://172.10.5.81:443";
 
-  Article({required this.title, required this.description});
-}
+class Bookmark {
+  final String refLink;
+  final String refTitle;
 
-class ScrapScreen extends StatelessWidget {
-  final List<Article> articles = [
-    Article(
-      title: "Article 1",
-      description: "This is the description of Article 1.",
-    ),
-    Article(
-      title: "Article 2",
-      description: "This is the description of Article 2.",
-    ),
-    Article(
-      title: "Article 3",
-      description: "This is the description of Article 3.",
-    ),
-    // Add more articles as needed
-  ];
+  Bookmark({required this.refLink, required this.refTitle});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(articles[index].title),
-            subtitle: Text(articles[index].description),
-            onTap: () {
-              // You can add navigation to the article detail page here if needed
-              // For this example, we will just print the article title to the console.
-              print("Article title: ${articles[index].title}");
-            },
-          );
-        },
-      ),
+  factory Bookmark.fromJson(Map<String, dynamic> json) {
+    return Bookmark(
+      refLink: json['refLink'],
+      refTitle: json['refTitle'],
     );
   }
 }
+//
+
+class ScrapScreen extends StatefulWidget {
+  @override
+  _ScrapScreenState createState() => _ScrapScreenState();
+}
+
+class _ScrapScreenState extends State<ScrapScreen> {
+
+  void updateBookmarks() {
+    fetchBookmarks();
+  }
+
+  Future<String?> getJwtToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('jwtToken');
+  }
+
+  Future<List<Bookmark>> fetchBookmarks() async {
+    final jwtToken = await getJwtToken();
+    final request =  Uri.parse("$baseUrl/bookmarks");
+    final headers = <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $jwtToken'
+    };
+    var response = await http.get(request, headers:headers);
+    var json = jsonDecode(response.body);
+    List<Bookmark> bookmarks = [];
+    // for(var bookmarkJson in json)
+    // {
+    //   bookmarks.add(Bookmark.fromJson(bookmarkJson));
+    // }
+    setState(() {
+      for(var bookmarkJson in json)
+      {
+        bookmarks.add(Bookmark.fromJson(bookmarkJson));
+      }
+    });
+    return bookmarks;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, height *0.02, 0, height *0.02),
+                  child: Text(
+                    "My bookmark list 😽",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+          FutureBuilder<List<Bookmark>>(
+            future:fetchBookmarks(),
+            builder: (BuildContext context, AsyncSnapshot<List<Bookmark>> snapshot){
+              if(snapshot.hasData) {
+                List<Bookmark> bookmarks = snapshot.data!;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: bookmarks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final item = bookmarks[index];
+                    return GestureDetector(
+                      onTap: ()  {
+                        // Navigator.push(
+                        // context,
+                        // MaterialPageRoute(builder: (context) => (item)),
+                        // );
+                      },
+
+                      child: Container(
+                        width: width*0.9,
+                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xFFE0E0E0)),
+                          borderRadius: BorderRadius.circular(8.0),
+                          // image: DecorationImage(
+                          //   image: AssetImage('assets/images/beforeselect.jpg'),
+                          //   fit: BoxFit.cover,
+                          // ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 8),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start, // Adjust crossAxisAlignment
+                              children: [
+                                Flexible(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${item.refTitle}",
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18
+                                        ),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      SizedBox(height: height * 0.005),
+                                      Text(
+                                        "${item.refLink}",
+                                        style: const TextStyle(
+                                        // fontWeight: FontWeight.bold,
+                                        fontSize: 16
+                                        ),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+              else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return CircularProgressIndicator();
+              }
+            }
+
+    ),
+              // Container(
+              //   child: ElevatedButton(
+              //       onPressed: fetchBookmarks,
+              //       child: Icon(Icons.refresh),
+              //   )
+              // )
+
+            ],
+          )
+        )
+      ],
+
+    );
+  }
+}
+
+
+
+
+
--- lib/search.dart
+++ lib/search.dart
@@ -7,7 +7,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-
+// String baseUrl = "http://localhast:8080";
 String baseUrl = "http://172.10.5.81:443";
 
 class Article {
@@ -59,6 +59,8 @@ class _SearchScreenState extends State<SearchScreen> {
       'keywords': keywords.isNotEmpty ? keywords : 'default_keywords_here' // 검색어가 비어있으면 기본 값 전달
     };
     final uri = Uri.http('172.10.5.81:443', '/articles', queryParams); // 쿼리 파라미터를 포함한 URL 생성
+    // final uri = Uri.http('127.0.0.1:8080', '/articles', queryParams); // 쿼리 파라미터를 포함한 URL 생성
+
     final request = '$baseUrl/articles?keywords="$keywords"';
     try {
       print(request);
